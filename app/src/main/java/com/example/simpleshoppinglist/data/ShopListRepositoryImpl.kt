4625b5a5c995ddaa87827d57e87ddcacd90104b6
Andrew <andrew.maneshin@gmail.com>
2022-03-26 17:11:35 +0300

    created data

---
+++ app/src/main/java/com/example/simpleshoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.example.simpleshoppinglist.data
+
+import com.example.simpleshoppinglist.domain.ShopItem
+import com.example.simpleshoppinglist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID)
+            shopItem.id = autoIncrementId++
+        shopList.add(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldShopItem = getShopItem(shopItem.id)
+        removeShopItem(oldShopItem)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("ShopItem id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+    override fun removeShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+}
